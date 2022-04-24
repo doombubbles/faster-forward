63ba6ac6c0178a3faff9b16eea25de3646325ab7
doombubbles <obsidiantoolzg@gmail.com>
2022-04-24 13:23:01 -0700

    File scoped namespace

--- FasterForwardMod.cs
+++ FasterForwardMod.cs
@@ -12,103 +12,102 @@ using UnityEngine;
 [assembly: MelonInfo(typeof(FasterForwardMod), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 
-namespace FasterForward
+namespace FasterForward;
+
+public class FasterForwardMod : BloonsTD6Mod
 {
-    public class FasterForwardMod : BloonsTD6Mod
+    private static int speed = 3;
+
+    private static readonly ModSettingHotkey Speed3 = new(KeyCode.F1)
     {
-        private static int speed = 3;
+        displayName = "3x Speed (default)"
+    };
 
-        private static readonly ModSettingHotkey Speed3 = new(KeyCode.F1)
+    private static readonly ModSettingHotkey Speed5 = new(KeyCode.F2)
+    {
+        displayName = "5x Speed"
+    };
+
+    private static readonly ModSettingHotkey Speed10 = new(KeyCode.F3)
+    {
+        displayName = "10x Speed"
+    };
+
+    private static readonly ModSettingHotkey Speed25 = new(KeyCode.F4)
+    {
+        displayName = "25x Speed"
+    };
+
+    private static readonly ModSettingHotkey SpeedCustom = new(KeyCode.F5)
+    {
+        displayName = "Custom Speed"
+    };
+
+    public override void OnUpdate()
+    {
+        var lastSpeed = speed;
+        if (Speed3.JustPressed())
         {
-            displayName = "3x Speed (default)"
-        };
-
-        private static readonly ModSettingHotkey Speed5 = new(KeyCode.F2)
-        {
-            displayName = "5x Speed"
-        };
-
-        private static readonly ModSettingHotkey Speed10 = new(KeyCode.F3)
-        {
-            displayName = "10x Speed"
-        };
-
-        private static readonly ModSettingHotkey Speed25 = new(KeyCode.F4)
-        {
-            displayName = "25x Speed"
-        };
-
-        private static readonly ModSettingHotkey SpeedCustom = new(KeyCode.F5)
-        {
-            displayName = "Custom Speed"
-        };
-
-        public override void OnUpdate()
-        {
-            var lastSpeed = speed;
-            if (Speed3.JustPressed())
-            {
-                speed = 3;
-            }
-
-            if (Speed5.JustPressed())
-            {
-                speed = 5;
-            }
-
-            if (Speed10.JustPressed())
-            {
-                speed = 10;
-            }
-
-            if (Speed25.JustPressed())
-            {
-                speed = 25;
-            }
-            
-            if (InGame.instance == null) return;
-
-            if (SpeedCustom.JustPressed())
-            {
-                PopupScreen.instance.ShowSetValuePopup("Custom Fast Forward Speed",
-                    "Sets the Fast Forward speed to the specified value",
-                    new Action<int>(i =>
-                    {
-                        if (i < 1)
-                        {
-                            i = 1;
-                        }
-
-                        if (i > 100)
-                        {
-                            i = 100;
-                        }
-
-                        speed = i;
-                        Game.instance.ShowMessage(
-                            "Fast Forward Speed is now " + speed + "x" + (speed == 3 ? " (Default)" : ""), 1f);
-                    }), speed);
-            }
-
-            if (speed != lastSpeed)
-            {
-                Game.instance.ShowMessage("Fast Forward Speed is now " + speed + "x" + (speed == 3 ? " (Default)" : ""),
-                    1f);
-            }
-
-
-            if (TimeManager.FastForwardActive)
-            {
-                TimeManager.timeScaleWithoutNetwork = speed;
-                TimeManager.networkScale = speed;
-            }
-            else
-            {
-                TimeManager.timeScaleWithoutNetwork = 1;
-                TimeManager.networkScale = 1;
-            }
-
-            TimeManager.maxSimulationStepsPerUpdate = speed;
+            speed = 3;
         }
+
+        if (Speed5.JustPressed())
+        {
+            speed = 5;
+        }
+
+        if (Speed10.JustPressed())
+        {
+            speed = 10;
+        }
+
+        if (Speed25.JustPressed())
+        {
+            speed = 25;
+        }
+            
+        if (InGame.instance == null) return;
+
+        if (SpeedCustom.JustPressed())
+        {
+            PopupScreen.instance.ShowSetValuePopup("Custom Fast Forward Speed",
+                "Sets the Fast Forward speed to the specified value",
+                new Action<int>(i =>
+                {
+                    if (i < 1)
+                    {
+                        i = 1;
+                    }
+
+                    if (i > 100)
+                    {
+                        i = 100;
+                    }
+
+                    speed = i;
+                    Game.instance.ShowMessage(
+                        "Fast Forward Speed is now " + speed + "x" + (speed == 3 ? " (Default)" : ""), 1f);
+                }), speed);
+        }
+
+        if (TimeManager.FastForwardActive)
+        {
+            TimeManager.timeScaleWithoutNetwork = speed;
+            TimeManager.networkScale = speed;
+        }
+        else
+        {
+            TimeManager.timeScaleWithoutNetwork = 1;
+            TimeManager.networkScale = 1;
+        }
+
+        TimeManager.maxSimulationStepsPerUpdate = speed;
+
+        if (speed != lastSpeed)
+        {
+            Game.instance.ShowMessage("Fast Forward Speed is now " + speed + "x" + (speed == 3 ? " (Default)" : ""),
+                1f);
+        }
+
     }
 }
